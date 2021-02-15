e9964763ce44b782c05a8e3ed08f1cad37e0926a
jack <16585824+beefpatty8888@users.noreply.github.com>
2021-02-14 20:33:23 -0500

    Add initial bootstrap CSS files and a parameter in the flask app to disable caching.

--- app/app.py
+++ app/app.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask import render_template
 app = Flask(__name__)
+# https://stackoverflow.com/questions/34066804/disabling-caching-in-flask
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 name = "The King - Elvis Presley"
 
 @app.route('/')
